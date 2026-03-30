b8ebaac3a5a32b566967d4fd215051d6be22324b
SigKun <mohammed_jamil1@msn.com>
2026-03-30 12:53:59 +0300

    .
    .

--- Scripts/capturure.gd
+++ Scripts/capturure.gd
@@ -1,15 +1,15 @@
 extends Area3D
 
 var points
-@onready var up: Area3D = $"../Up"
-@onready var right: Area3D = $"../Right"
-@onready var left: Area3D = $"../Left"
-@onready var down: Area3D = $"../Down"
+#@onready var up: Area3D = $"../Up"
+#@onready var right: Area3D = $"../Right"
+#@onready var left: Area3D = $"../Left"
+#@onready var down: Area3D = $"../Down"
 
-var downBeat = preload("res://Scenes/Beats/boxrureDown.tscn")
-var leftBeat = preload("res://Scenes/Beats/boxrureLeft.tscn")
-var rightBeat = preload("res://Scenes/Beats/boxrureRight.tscn")
-var upBeat = preload("res://Scenes/Beats/boxrureUp.tscn")
+#var downBeat = preload("res://Scenes/Beats/boxrureDown.tscn")
+#var leftBeat = preload("res://Scenes/Beats/boxrureLeft.tscn")
+#var rightBeat = preload("res://Scenes/Beats/boxrureRight.tscn")
+#var upBeat = preload("res://Scenes/Beats/boxrureUp.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,19 +23,20 @@ func _process(delta: float) -> void:
 	
 
 func handleBeats():
-	if overlaps_area(upBeat) and Input.is_action_just_pressed("Up"):
-		print("Touched UP Beat")
-		onCorrectTiming()
-		upBeat.queue_free()
-	if overlaps_area(downBeat) and Input.is_action_just_pressed("Down"):
-		onCorrectTiming()
-		downBeat.queue_free()
-	if overlaps_area(leftBeat) and Input.is_action_just_pressed("Left"):
-		onCorrectTiming()
-		leftBeat.queue_free()
-	if overlaps_area(rightBeat) and Input.is_action_just_pressed("Right"):
-		onCorrectTiming()
-		rightBeat.queue_free()
+	#if overlaps_area(upBeat) and Input.is_action_just_pressed("Up"):
+		#print("Touched UP Beat")
+		#onCorrectTiming()
+		#upBeat.queue_free()
+	#if overlaps_area(downBeat) and Input.is_action_just_pressed("Down"):
+		#onCorrectTiming()
+		#downBeat.queue_free()
+	#if overlaps_area(leftBeat) and Input.is_action_just_pressed("Left"):
+		#onCorrectTiming()
+		#leftBeat.queue_free()
+	#if overlaps_area(rightBeat) and Input.is_action_just_pressed("Right"):
+		#onCorrectTiming()
+		#rightBeat.queue_free()
+	pass
 
 
 func onCorrectTiming():
